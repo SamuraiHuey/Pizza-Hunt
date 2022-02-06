b8976eb545380bebf636cd2dd977728be4dad033
Will B <tryarc@comcast.net>
2022-02-06 02:23:31 -0500

    added pizza routes

---
+++ routes/api/pizza-routes.js
@@ -0,0 +1,26 @@
+const { route } = require('..');
+
+const router = require('express').Router();
+
+const {
+    getAllPizza,
+    getPizzaById,
+    createPizza,
+    updatePizza,
+    deletePizza
+} = require('../../controllers/pizza-controllers');
+
+// Set up GET all and POST at /api/pizzas
+router
+    .route('/')
+    .get(getAllPizza)
+    .post(createPizza);
+
+// Set up GET on, PUT, and DELET at /api/pizzas/:id
+router
+    .route('/:id')
+    .get(getPizzaById)
+    .put(updatePizza)
+    .delete(deletePizza);
+
+module.exports = router;
